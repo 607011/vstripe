f28df119af3771650762dee0dd8771b0f7e1367a
Oliver Lau <oliver@von-und-fuer-lau.de>
2011-06-13 16:49:23 +0200

    .

--- VStripe.pro
+++ VStripe.pro
@@ -28,8 +28,7 @@ FFMPEGDIR = /opt/local
 OPENCVDIR = /opt/local
 ICON = VStripe.icns
 QMAKE_INFO_PLIST = VStripe.plist
-QMAKE_CXXFLAGS += -msse -msse2 -msse3
-# QMAKE_CXXFLAGS += -static -fvisibility=hidden
+# QMAKE_CXXFLAGS += -msse -msse2 -msse3 -msse4.1 -msse4.2
 LIBS += -L$${FFMPEGDIR}/lib \
     -lavformat \
     -lavcodec \
--- deploymac.sh
+++ deploymac.sh
@@ -15,62 +15,65 @@ QTLIBDIR=$QTDIR/lib
 # ----------------------------------------
 APPBUNDLE=./$APP.app
 
-#rm -Rf $APPBUNDLE
 rm $APP-$VERSION.dmg
 
-#qmake CONFIG=release
-#make clean
-#make 
+rm -Rf $APPBUNDLE
+
+qmake CONFIG+=release
+make clean
+make 
+
+echo Making translations ..
+$QTDIR/bin/lrelease VStripe.pro
+cp $APP*.qm $APPBUNDLE/Contents/MacOS
 
 echo Bundling libs ..
+
 dylibbundler -od -b -x $APPBUNDLE/Contents/MacOS/$APP -d $APPBUNDLE/Contents/libs
 
 echo Creating directories for Qt frameworks ..
-mkdir $APPBUNDLE/Contents
-mkdir $APPBUNDLE/Contents/Frameworks
-mkdir $APPBUNDLE/Contents/Frameworks/QtCore.framework
-mkdir $APPBUNDLE/Contents/Frameworks/QtGui.framework
-mkdir $APPBUNDLE/Contents/Frameworks/QtXml.framework
-mkdir $APPBUNDLE/Contents/Frameworks/QtCore.framework/Versions
-mkdir $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions
-mkdir $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions
-mkdir $APPBUNDLE/Contents/Frameworks/QtCore.framework/Versions/4
-mkdir $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions/4
-mkdir $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions/4
+
+mkdir -pv $APPBUNDLE/Contents/Frameworks/QtCore.framework/Versions/4
+mkdir -pv $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions/4
+mkdir -pv $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions/4
 
 echo Copying Qt framework resources to application bundle ..
+
 cp -Rv $QTLIBDIR/QtCore.framework/Versions/4/Resources $APPBUNDLE/Contents/Frameworks/QtCore.framework
 cp -Rv $QTLIBDIR/QtGui.framework/Versions/4/Resources $APPBUNDLE/Contents/Frameworks/QtGui.framework
 cp -Rv $QTLIBDIR/QtXml.framework/Versions/4/Resources $APPBUNDLE/Contents/Frameworks/QtXml.framework
 
 echo Copying Qt frameworks to application bundle ..
+
 cp -v $QTLIBDIR/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/Frameworks/QtCore.framework/Versions/4
 cp -v $QTLIBDIR/QtGui.framework/Versions/4/QtGui $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions/4
 cp -v $QTLIBDIR/QtXml.framework/Versions/4/QtXml $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions/4
 
-echo Changing paths to frameworks in $APP ..
+echo Fixing dependencies on $APP ..
 
-install_name_tool -change $QTLIBDIR/QtCore.framework/Versions/Current/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/MacOS/VStripe
+install_name_tool -change $QTLIBDIR/QtCore.framework/Versions/Current/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/MacOS/$APP
 
-install_name_tool -change $QTLIBDIR/QtGui.framework/Versions/Current/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $APPBUNDLE/Contents/MacOS/VStripe
+install_name_tool -change $QTLIBDIR/QtGui.framework/Versions/Current/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $APPBUNDLE/Contents/MacOS/$APP
 
-install_name_tool -change $QTLIBDIR/QtXml.framework/Versions/Current/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $APPBUNDLE/Contents/MacOS/VStripe
+install_name_tool -change $QTLIBDIR/QtXml.framework/Versions/Current/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $APPBUNDLE/Contents/MacOS/$APP
 
-echo Changing paths to frameworks in QtCore ..
+echo Fixing dependencies on QtCore ..
 
 install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/Frameworks/QtCore.framework/Versions/4/QtCore
 
-echo Changing paths to frameworks in QtXml ..
+echo Fixing dependencies on QtXml ..
 
 install_name_tool -id @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions/4/QtXml
 
 install_name_tool -change $QTLIBDIR/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/Frameworks/QtXml.framework/Versions/4/QtXml
 
-echo Changing paths to frameworks in QtGui ..
+echo Fixing dependencies on QtGui ..
 
 install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions/4/QtGui
 
 install_name_tool -change $QTLIBDIR/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore $APPBUNDLE/Contents/Frameworks/QtGui.framework/Versions/4/QtGui
 
-echo Generating DMG ..
+
+echo Generating disk image ..
+
 hdiutil create -srcfolder "$APPBUNDLE" -volname "$APP $VERSION" -fs HFS+ -format UDZO -size 100M "$APP-$VERSION.dmg"
